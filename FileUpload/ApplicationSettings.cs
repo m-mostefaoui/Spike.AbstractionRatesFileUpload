93e0e4dfad08a0639272c3674a2869d75e9db8e1
Mostefaoui <m.mostefaoui@hotmail.fr>
2018-10-05 14:33:09 +0100

    Initial

---
+++ FileUpload/ApplicationSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace FileUpload
+{
+    using System;
+    using Microsoft.Extensions.Configuration;
+
+    public static class ApplicationSettings
+    {
+        public static string FileUploadBaseUrl { get; }
+        public static string SharedDirectory { get; }
+
+
+        static ApplicationSettings()
+        {
+            var settingsResolver = GetSettingsResolver();
+            FileUploadBaseUrl = settingsResolver("Spike.FileUpload.BaseUrl");
+            SharedDirectory = settingsResolver("Spike.SharedDirectory");
+        }
+
+        private static Func<string, string> GetSettingsResolver()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+            var configuration = configurationBuilder.Build();
+
+            return (name) => configuration.GetSection(name).Value;
+        }
+    }
+}
---
+++ FileUpload/Controllers/FileUploadsController.cs
@@ -0,0 +1,38 @@
+﻿namespace FileUpload.Controllers
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadsController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+
+            var filePathsList = new List<string>();
+
+            foreach (var formFile in files)
+            {
+                var filePath = Path.Combine(ApplicationSettings.SharedDirectory, formFile.FileName);
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+
+                filePathsList.Add(filePath);
+            }
+
+            return Ok(new {count = files.Count, size, filePathsList});
+        }
+    }
+}
